3dc987f2f2e70a5bac556a3aa6e0f075f1eb12c7
hlky <hlky@hlky.ac>
2024-05-07 19:49:39 +0100

    update esrgan

--- examples/08_esrgan/demo.py
+++ examples/08_esrgan/demo.py
@@ -14,17 +14,21 @@
 #
 import click
 import numpy as np
+import safetensors.torch
 import torch
 
 from aitemplate.compiler import Model
+from aitemplate.testing.benchmark_pt import benchmark_torch_function
 from PIL import Image
+from modeling.rrdbnet_pt import RRDBNet
 
 
 def esrgan_inference(
     exe_module: Model,
     input_pixels: np.ndarray,
     scale=4,
-) -> torch.Tensor:
+    benchmark: bool = False,
+) -> torch.Tensor | float:
     if np.max(input_pixels) > 256:
         max_range = 65535
     else:
@@ -44,14 +48,18 @@ def esrgan_inference(
         shape[1] = height * scale
         shape[2] = width * scale
         ys[name] = torch.empty(shape).cuda().half()
-    exe_module.run_with_tensors(inputs, ys, graph_mode=False)
-    upscaled = ys["upscaled_pixels"]
-    upscaled = upscaled.squeeze(0).cpu().clamp_(0, 1).numpy()
-    if max_range == 65535:
-        upscaled = (upscaled * 65535.0).round().astype(np.uint16)
+    if benchmark:
+        t, _, _ = exe_module.benchmark_with_tensors(inputs, ys, count=25, repeat=2)
+        return t
     else:
-        upscaled = (upscaled * 255.0).round().astype(np.uint8)
-    return upscaled
+        exe_module.run_with_tensors(inputs, ys, graph_mode=False)
+        upscaled = ys["upscaled_pixels"]
+        upscaled = upscaled.squeeze(0).cpu().clamp_(0, 1).numpy()
+        if max_range == 65535:
+            upscaled = (upscaled * 65535.0).round().astype(np.uint16)
+        else:
+            upscaled = (upscaled * 255.0).round().astype(np.uint8)
+        return upscaled
 
 
 @click.command()
@@ -75,20 +83,62 @@ def esrgan_inference(
     default=4,
     help="Scale of ESRGAN model",
 )
+@click.option(
+    "--benchmark",
+    is_flag=True,
+    help="Benchmark mode",
+)
+@click.option(
+    "--model-path",
+    default="RealESRGAN_x4plus.pth",
+    help="model path. supports torch or safetensors",
+)
+@click.option(
+    "--size",
+    default=256,
+    help="size of input image",
+)
 def demo(
     module_path,
     input_image_path,
     output_image_path,
     scale,
+    benchmark,
+    model_path,
+    size,
 ):
     module = Model(module_path)
-    input_image = Image.open(input_image_path).convert("RGB")
+    input_image = Image.open(input_image_path).convert("RGB").resize((size, size))
     image_array = np.array(input_image)
 
-    upscaled = esrgan_inference(module, image_array, scale)
+    upscaled = esrgan_inference(module, image_array, scale, benchmark=benchmark)
+    if benchmark:
+        ait_t = upscaled
+    else:
+        output_image = Image.fromarray(upscaled)
+        output_image.save(output_image_path)
+        return
 
-    output_image = Image.fromarray(upscaled)
-    output_image.save(output_image_path)
+    if model_path.endswith(".safetensors"):
+        pt_model = safetensors.torch.load_file(model_path)
+    else:
+        pt_model = torch.load(model_path)
+
+    if "params_ema" in pt_model.keys():
+        pt_model = pt_model["params_ema"]
+    elif "params" in pt_model.keys():
+        pt_model = pt_model["params"]
+
+    del module
+    pt_module = RRDBNet(3, 3, scale=scale)
+    pt_module.load_state_dict(pt_model)
+    pt_module.eval()
+    pt_module = pt_module.cuda().half()
+    pt_input = torch.from_numpy(image_array).unsqueeze(0).permute((0, 3, 1, 2)).contiguous().cuda().half()
+    pt_t = benchmark_torch_function(25, pt_module.forward, pt_input)
+    print(f"AIT: {ait_t:.2f} ms")
+    print(f"PyTorch: {pt_t:.2f} ms")
+
 
 
 if __name__ == "__main__":
